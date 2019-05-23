8eb5ec46e02ec9ed65529e9c5de6e93eaecccf2e
Yingge Hu <ayanamirei629@gmail.com>
2019-05-23 16:11:16 -0400

    Simple node server

---
+++ app.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send(`Hello world!`);
+});
+
+app.get('/about',(req, res) =>{
+	res.send(`I like long wals on the beach.`);
+})
+
+app.listen(4000,() => console.log('Listening on 4000'));
